c2efbf864c71f5ed3e41e0d9bf26bfb40b70aa81
xiaodongaa <127294923+xiaodongaa@users.noreply.github.com>
2023-03-08 16:47:01 +0800

    project1

---
+++ project1/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace DemoUser
+{
+    public class User
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public User(string userEmail)
+        {
+            m_email = userEmail;
+        }
+        private string m_email;
+    }
+}
